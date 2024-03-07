53925e6cb6365da0f891627401d3f6283435b8a7
Ebrahim-Gamal-77 <149944484+Ebrahim-Gamal-77@users.noreply.github.com>
2024-03-07 04:55:27 +0200

    Create Audio player (WAV).java

---
+++ Audio player (WAV).java
@@ -0,0 +1,49 @@
+package learning;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.sound.sampled.*;
+
+public class Main {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+        File file = new File("G:\\سورة الانسان الشيخ حسن صالح.wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        Clip clip = AudioSystem.getClip();
+        clip.open(audioStream);
+        
+        String response = "";
+        while (!response.equals("Q")){
+
+            System.out.println("P = Play , S = Stop , R = Reset , Quit = Q");
+            System.out.print("Enter your choice: ");
+            response = scanner.next().toUpperCase();
+
+            switch(response){
+                case ("P"):
+                    clip.start();
+                    System.out.println("The sound is playing\n");
+                    break;
+                case ("S"):
+                    clip.stop();
+                    System.out.println("The sound stopped\n");
+                    break;
+                case ("R"):
+                    clip.setMicrosecondPosition(0);
+                    System.out.println("The sound reset\n");
+                    break;
+                case ("Q"):
+                    clip.close();
+                    System.out.println("Bye\n");
+                    break;
+                default:
+                    System.out.println("Not a valid response\n");
+            }
+            
+        }
+
+    }
+
+}
