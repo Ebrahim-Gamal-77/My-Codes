c1e5b945ac2045fb44f41f6546e13bace229951b
Ebrahim-Gamal-77 <149944484+Ebrahim-Gamal-77@users.noreply.github.com>
2024-03-10 02:50:49 +0200

    Update Audio player (WAV).java

--- Audio player (WAV).java
+++ Audio player (WAV).java
@@ -6,42 +6,44 @@ import java.util.Scanner;
 import javax.sound.sampled.*;
 
 public class Main {
-    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
+    public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 
-        Scanner scanner = new Scanner(System.in);
-        File file = new File("G:\\سورة الانسان الشيخ حسن صالح.wav");
-        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
-        Clip clip = AudioSystem.getClip();
-        clip.open(audioStream);
-        
-        String response = "";
-        while (!response.equals("Q")){
+        try(Scanner scanner = new Scanner(System.in)) {
+            File file = new File("G:\\سورة الانسان الشيخ حسن صالح.wav");
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
 
-            System.out.println("P = Play , S = Stop , R = Reset , Quit = Q");
-            System.out.print("Enter your choice: ");
-            response = scanner.next().toUpperCase();
+            String response = "";
+            while (!response.equals("Q")) {
 
-            switch(response){
-                case ("P"):
-                    clip.start();
-                    System.out.println("The sound is playing\n");
-                    break;
-                case ("S"):
-                    clip.stop();
-                    System.out.println("The sound stopped\n");
-                    break;
-                case ("R"):
-                    clip.setMicrosecondPosition(0);
-                    System.out.println("The sound reset\n");
-                    break;
-                case ("Q"):
-                    clip.close();
-                    System.out.println("Bye\n");
-                    break;
-                default:
-                    System.out.println("Not a valid response\n");
+                System.out.println("P = Play , S = Stop , R = Reset , Quit = Q");
+                System.out.print("Enter your choice: ");
+                response = scanner.next().toUpperCase();
+
+                switch (response) {
+                    case "P":
+                        clip.start();
+                        System.out.println("The sound is playing\n");
+                        break;
+                    case "S":
+                        clip.stop();
+                        System.out.println("The sound stopped\n");
+                        break;
+                    case "R":
+                        clip.setMicrosecondPosition(0);
+                        System.out.println("The sound reset\n");
+                        break;
+                    case "Q":
+                        clip.close();
+                        System.out.println("Bye");
+                        break;
+                    default:
+                        System.out.println("Not a valid response\n");
+                }
+
             }
-            
+
         }
 
     }
